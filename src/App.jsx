11eff4fa22601b899713d97a11039db75dd9f8aa
refinmaldini <refin22@gmail.com>
2024-03-06 21:01:38 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Nav from "./components/Nav";
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import SuperQuality from "./sections/SuperQuality";
+
+const App = () => {
+  return (
+    <>
+      <main className="relative">
+        <Nav />
+        <section className="xl:padding-l wide:padding-r padding-b">
+          <Hero />
+        </section>
+        <section className="padding">
+          <PopularProducts />
+        </section>
+        <section className="padding">
+          <SuperQuality />
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default App;
